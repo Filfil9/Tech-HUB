8ed6664d667adde84537e87f9a865f7066d291d4
rahaf aws <166960462+Filfil9@users.noreply.github.com>
2025-07-29 13:11:21 +0300

    add routing for posts and restructure App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import { Routes, Route } from "react-router-dom";
+import Home from "./Task1/Home";
+import About from "./Task1/About";
+import Services from "./Task1/Services";
+import Contact from "./Task1/Contact";
+import WhatILearned from "./Task1/WhatILearned";
+import Header from "./Task1/Header";
+import Footer from "./Task1/Footer";
+import Posts from "./Task1/Post";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Header />
 
-export default App
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Task 1/Home';
+import About from './Task 1/About';
+import Services from './Task 1/Services';
+import Contact from './Task 1/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/what-i-learned" element={<WhatILearned />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+  );
+}
+export default App;
+
